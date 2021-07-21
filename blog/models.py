1eb44399815357007921d3694024bd8bf7747905
Lee, Chang Wook <lee857@indiana.edu>
2021-07-21 17:02:39 +0900

    Added jobs page and images are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog models
+class Blog(models.Model):
+	# title
+	title = models.CharField(max_length=255)
+	# pub_date
+	pub_date = models.DateTimeField()
+	# body
+	body = models.TextField()
+	# image
+	image = models.ImageField(upload_to='images/')
+
+# Add the Blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
